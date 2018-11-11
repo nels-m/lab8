478dd45e150e6cbbd0e23b57923f4faa751eb596
skitRobot <nels.murray@gmail.com>
2018-11-11 22:54:37 +0000

    third commit

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template, Markup, request, url_for
 MyApp = Flask(__name__)
 
 @MyApp.route('/')
-def landing():
-	return '<h1>This is a landing page for COSC419 Lab8. Here is more.</h1>'
+def index():
+	return render_template('index.html')
+
 
 if __name__ == "__main__":
         MyApp.run()
