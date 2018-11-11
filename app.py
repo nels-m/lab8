113fb247f152e8d4f144f4f2f1f61520cc2bf7e5
skitRobot <nels.murray@gmail.com>
2018-11-11 20:07:41 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, Markup, request, url_for
+
+MyApp = Flask(__name__)
+
+@MyApp.route('/')
+def landing():
+	return '<h1>This is a landing page for COSC419 Lab8</h1>'
+
+if __name__ == "__main__":
+        MyApp.run()
+
