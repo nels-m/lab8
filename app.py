8c6000437cb5b543a8f364c2e774c9aa4a26bf30
skitRobot <nels.murray@gmail.com>
2018-11-12 01:18:06 +0000

    fifth commit

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, Markup, request, url_for
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 MyApp = Flask(__name__)
 MyApp.config['SECRET_KEY'] = 'tistbas123'
+Bootstrap(MyApp)
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
 	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 	remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+	email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+        password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 @MyApp.route('/')
 def index():
@@ -19,6 +26,11 @@ def index():
 def login():
 	form = LoginForm()
 	return render_template('login.html', form=form)
+
+@MyApp.route('/signup')
+def signup():
+	form = RegisterForm()
+	return render_template('signup.html', form=form)
 
 
 if __name__ == "__main__":
