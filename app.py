02e1381ad6da5b081e0a3f6802c90e92c1d2e14c
skitRobot <nels.murray@gmail.com>
2018-11-12 04:52:18 +0000

    seventh commit

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, Markup, request, url_for
+from flask import Flask, render_template, Markup, request, url_for, redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 MyApp = Flask(__name__)
 MyApp.config['SECRET_KEY'] = 'tistbas123'
+MyApp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/db/lab8/lab8data.db'
 Bootstrap(MyApp)
+db = SQLAlchemy(MyApp)
+login_manager = LoginManager()
+login_manager.init_app(MyApp)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True)
+	email = db.Column(db.String(50), unique=True)
+	password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -22,16 +40,44 @@ class RegisterForm(FlaskForm):
 def index():
 	return render_template('index.html')
 
-@MyApp.route('/login')
+@MyApp.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
+				return redirect(url_for('home'))
+
+		return '<h1>Invalid username or password</h1>'
+
 	return render_template('login.html', form=form)
 
-@MyApp.route('/signup')
+@MyApp.route('/signup', methods=['GET', 'POST'])
 def signup():
 	form = RegisterForm()
+	
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method='sha256')
+        	new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+		return '<h1>New user has been created!</h1>'
+
 	return render_template('signup.html', form=form)
 
+@MyApp.route('/home')
+@login_required
+def home():
+	return render_template('home.html', name=current_user.username)
+
+@MyApp.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 if __name__ == "__main__":
         MyApp.run()
